51cbb39b7d2b479ae71975fcc193c3a32843c277
Chris Mowforth <chris.mowforth@hardpoint.dev>
2026-03-24 17:37:27 +0400

    move config types to separate file

---
+++ config.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Service config file
+type ServiceConfig struct {
+	Name string            `json:"name"`
+	Host string            `json:"host"`
+	Port int16             `json:"port"`
+	Tags map[string]string `json:"tags"`
+}
+
+type AgentConfig struct {
+	OrgId    string          `json:"org_id"`
+	Services []ServiceConfig `json:"services"`
+}
+
+func (ac *AgentConfig) String() string {
+	return fmt.Sprintf("Org ID: %s", ac.OrgId)
+}
+
+// CLI Arguments
+type Args struct {
+	SkipTls       bool           `arg:"--skip-tls,env:SKIP_TLS" default:"false" help:"Bypass TLS certificate validation"`
+	Relay         string         `arg:"env" default:"relay.hardpoint.dev:443" help:"Relay endpoint"`
+	Config        string         `arg:"required,env" help:"Path to configuration file"`
+	KeyDir        string         `arg:"key-dir" default:"/var/lib/hardpoint"`
+	ServiceConfig *ServiceConfig `arg:"-"`
+}
+
+func parseServiceConfig(args Args) (*ServiceConfig, error) {
+	file, err := os.Open(args.Config)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	var serviceConfig *ServiceConfig
+	err = decoder.Decode(&serviceConfig)
+	if err != nil {
+		return nil, err
+	}
+	return serviceConfig, nil
+}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -17,44 +16,6 @@ import (
 	"github.com/quic-go/quic-go/qlog"
 	"golang.org/x/sync/errgroup"
 )
-
-// Service config file
-type ServiceConfig struct {
-	Name string            `json:"name"`
-	Host string            `json:"host"`
-	Port int16             `json:"port"`
-	Tags map[string]string `json:"tags"`
-}
-
-type AgentConfig struct {
-	OrgId    string          `json:"org_id"`
-	Services []ServiceConfig `json:"services"`
-}
-
-// CLI Arguments
-type Args struct {
-	SkipTls       bool           `arg:"--skip-tls,env:SKIP_TLS" default:"false" help:"Bypass TLS certificate validation"`
-	Relay         string         `arg:"env" default:"relay.hardpoint.dev:443" help:"Relay endpoint"`
-	Config        string         `arg:"required,env" help:"Path to configuration file"`
-	KeyDir        string         `arg:"key-dir",env:"KEY_DIR" default:"/var/lib/hardpoint"`
-	ServiceConfig *ServiceConfig `arg:"-"`
-}
-
-func parseServiceConfig(args Args) (*ServiceConfig, error) {
-	file, err := os.Open(args.Config)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	var serviceConfig *ServiceConfig
-	err = decoder.Decode(&serviceConfig)
-	if err != nil {
-		return nil, err
-	}
-	return serviceConfig, nil
-}
 
 func clientMain(args Args) error {
 	tlsConf := &tls.Config{
