8042a71e65cb65f89b3fad6397b9a2811a4db9ef
Chris Mowforth <chris.mowforth@hardpoint.dev>
2026-04-11 00:40:23 +0400

    fix: align paths in go config

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ func (ac *AgentConfig) String() string {
 type Args struct {
 	SkipTls     bool         `arg:"--skip-tls,env:SKIP_TLS" default:"false" help:"Bypass TLS certificate validation"`
 	Relay       string       `arg:"env" default:"relay.hardpoint.dev:443" help:"Relay endpoint"`
-	KeyDir      string       `default:"/var/lib/hardpoint"`
+	KeyDir      string       `default:"/var/lib/hardpointd"`
 	AgentConfig *AgentConfig `arg:"-"`
 	ListenCmd   *ListenCmd   `arg:"subcommand:listen" help:"Start the agent and listen for connections"`
 	Config      string       `arg:"--config,env" default:"/etc/hardpointd/config.yaml" help:"Path to configuration file"`
